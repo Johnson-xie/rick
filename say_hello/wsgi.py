2cfce7725c6420d5077d191c8edac0cf9e9bdd2d
Johnson_xie <johnson19921005@163.com>
2020-05-24 18:14:12 +0800

    wsgi

--- say_hello/wsgi.py
+++ say_hello/wsgi.py
@@ -7,25 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'say_hello.settings')
-
-# application = get_wsgi_application()
-
 import os
 from os.path import dirname, abspath
 import sys
+from django.core.wsgi import get_wsgi_application
 
 
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
-
+print(PROJECT_DIR)
 
 sys.path.insert(0, PROJECT_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "say_hello.settings")
 
-from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
